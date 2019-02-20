894c73447048fd53f6db6e5d50c8829f5eb2f74a
Thomas Barregren <thomas@kntnt.se>
2019-02-20 11:59:03 +0100

    Added a check that get_use_by() don't rturn false.

--- kntnt-user_nicename-from-display_name.php
+++ kntnt-user_nicename-from-display_name.php
@@ -17,14 +17,14 @@ defined('ABSPATH') || die;
 
 add_filter('pre_user_nicename', function ($user_nicename) {
 
-    // Get user's display name
-    $name = get_user_by('slug', $user_nicename)->get('display_name');
-
     // Convert display name into a slug (a.k.a. user_nicename) by removing
-    // diacritics and replacing other "offending" characters with dashes.
-    $name = sanitize_user($name);
-    $name = sanitize_title_with_dashes($name);
+    // diacritics and replacing othe "offending" characters with dashes.
+    if ($user = get_user_by('slug', $user_nicename)) {
+        $user_nicename = $user->get('display_name');
+        $user_nicename = sanitize_user($user_nicename);
+        $user_nicename = sanitize_title_with_dashes($user_nicename);
+    }
 
-    return $name;
+    return $user_nicename;
 
 });
