366505ed50680faccff8770af8f323e9177f10a1
Thomas Barregren <thomas@kntnt.se>
2019-08-23 15:57:16 +0200

    Updated.

--- kntnt-user_nicename-from-display_name.php
+++ kntnt-user_nicename-from-display_name.php
@@ -12,32 +12,32 @@
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
  */
 
-defined('ABSPATH') || die;
+defined( 'ABSPATH' ) || die;
 
 /*
  * We cannot use the `pre_user_nicename` filter because we need to get
  * the display name and cannot use `get_user_by('slug', $user_nicename)`
  * on not yet created users (only on updates). Thus this filter instead.
  */
-add_filter('wp_pre_insert_user_data', function ($data, $update, $id) {
+add_filter( 'wp_pre_insert_user_data', function ( $data, $update, $id ) {
 
-    $user_nicename = $data['display_name'];
+	$user_nicename = $data['display_name'];
 
-    // Create`user_nicename` from `display_name` by removing diacritics  and
-    // replacing other "offending" characters with dashes.
-    $user_nicename = sanitize_user($user_nicename);
-    $user_nicename = sanitize_title_with_dashes($user_nicename);
+	// Create`user_nicename` from `display_name` by removing diacritics  and
+	// replacing other "offending" characters with dashes.
+	$user_nicename = sanitize_user( $user_nicename );
+	$user_nicename = sanitize_title_with_dashes( $user_nicename );
 
-    // Make the slug unique.
-    $n = 0;
-    $base = $user_nicename;
-    while (($test = get_user_by('slug', $user_nicename)) && $test->ID != $id) {
-        $n += 1;
-        $user_nicename = "$base-$n";
-    }
+	// Make the slug unique.
+	$n = 0;
+	$base = $user_nicename;
+	while ( ( $test = get_user_by( 'slug', $user_nicename ) ) && $test->ID != $id ) {
+		$n += 1;
+		$user_nicename = "$base-$n";
+	}
 
-    $data['user_nicename'] = $user_nicename;
+	$data['user_nicename'] = $user_nicename;
 
-    return $data;
+	return $data;
 
-}, 10, 3);
+}, 10, 3 );
